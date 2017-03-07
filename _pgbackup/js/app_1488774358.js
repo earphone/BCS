2fd9c07b3cde54539f882afebf83b3e877d9c587
brandon higashi <Higashi@brandons-MBP.hawaii.rr.com>
2017-03-06 20:36:49 -1000

    Changed backend from Kinvey to Google Firebase due to problems trying to initialize. Firebase seems to be better documented (probably because of Google).

---
+++ _pgbackup/js/app_1488774358.js
@@ -0,0 +1,45 @@
+x// based off of https://github.com/KinveyApps/HTML5-Starter/blob/master/www/js/app.js
+var app = {
+	events: {
+		// Login
+		'#login-button submit': 'login'
+	}//,
+	
+	login: function(event) {
+		// Prevent the form from being submitted
+		event.preventDefault();
+		
+		// Remove the login-error
+		$('#login-error').remove();
+		
+		// Login to Kinvey
+		Kinvey.User.login(email, password)
+			.then(function() {
+				location.replace('/');
+			})
+			.catch(function(error) {
+				$('#login-form').append('<p id="login-error" class="text-danger" style="margin-top: 10px;">' + error.message + '</p>');
+			});
+	}//,
+};
+
+// Initialize Kinvey
+Kinvey.initialize({
+    appKey: 'kid_ByrZLAIwl',
+    appSecret: '1c72c49283e14ca5a4191490a6ba4414'
+    alert("initializing");
+});
+/*.then(function(activeUser) {
+                    if (!activeUser && authorizedHrefs.indexOf(location.pathname) !== -1) {
+                      location.replace('/login.html');
+                    } else {
+                     return bindEvents();
+                    }
+                  })
+                  .then(function() {
+                    $('#loading-modal').modal('hide');
+                  })
+                  .catch(function(error) {
+                    console.log(error);
+                  });
+               */
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-x// based off of https://github.com/KinveyApps/HTML5-Starter/blob/master/www/js/app.js
+// based off of https://github.com/KinveyApps/HTML5-Starter/blob/master/www/js/app.js
 var app = {
 	events: {
 		// Login
@@ -11,35 +11,5 @@ var app = {
 		
 		// Remove the login-error
 		$('#login-error').remove();
-		
-		// Login to Kinvey
-		Kinvey.User.login(email, password)
-			.then(function() {
-				location.replace('/');
-			})
-			.catch(function(error) {
-				$('#login-form').append('<p id="login-error" class="text-danger" style="margin-top: 10px;">' + error.message + '</p>');
-			});
 	}//,
 };
-
-// Initialize Kinvey
-Kinvey.initialize({
-    appKey: 'kid_ByrZLAIwl',
-    appSecret: '1c72c49283e14ca5a4191490a6ba4414'
-    alert("initializing");
-});
-/*.then(function(activeUser) {
-                    if (!activeUser && authorizedHrefs.indexOf(location.pathname) !== -1) {
-                      location.replace('/login.html');
-                    } else {
-                     return bindEvents();
-                    }
-                  })
-                  .then(function() {
-                    $('#loading-modal').modal('hide');
-                  })
-                  .catch(function(error) {
-                    console.log(error);
-                  });
-               */
