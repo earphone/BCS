e6757664091983fe2e0fc2af26514fa0e7bc4a7c
earphone <bhigahi169@gmail.com>
2017-03-08 16:35:08 -1000

    Set up email and password for user creation.

---
+++ _pgbackup/js/app_1489026872.js
@@ -0,0 +1,16 @@
+// based off of https://github.com/KinveyApps/HTML5-Starter/blob/master/www/js/app.js
+var app = {
+	events: {
+		// Login
+		'#login-button submit': 'login'
+        '#create-employee-button submit': 'create-employee'
+	}//,
+	
+	login: function(event) {
+		// Prevent the form from being submitted
+		event.preventDefault();
+		
+		// Remove the login-error
+		$('#login-error').remove();
+	}//,
+};
